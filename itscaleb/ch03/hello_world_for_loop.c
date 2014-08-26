d654427919a6b3ead8932d1ddd05f72844a7bec8
Caleb Gossler <caleb.gossler@gmail.com>
2014-08-25 23:03:37 -0700

    hello world loops

---
+++ itscaleb/ch03/hello_world_for_loop.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+	for(int i = 0; i < 5; i++) {
+		puts("Hello, world!");
+	}
+    return 0;
+}
---
+++ itscaleb/ch03/hello_world_while_loop.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+	int i = 0;
+	while(i < 5) {
+		puts("Hello, world!");
+		i++;
+	}
+    return 0;
+}
