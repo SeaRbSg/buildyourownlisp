6666d537f1085fee13e47db5d6f4c24ec4a26a6f
Ryan Davis <ryand-ruby@zenspider.com>
2014-08-26 20:21:56 -0700

    Got ch05 actually parsing by cribbing from ch06.

--- zenspider/ch05.c
+++ zenspider/ch05.c
@@ -1,18 +1,35 @@
 #include "mpc.h"
+#include <editline/readline.h>
 
 int main() {
   mpc_parser_t* Adjective = mpc_new("adjective");
   mpc_parser_t* Noun      = mpc_new("noun");
   mpc_parser_t* Phrase    = mpc_new("phrase");
   mpc_parser_t* Doge      = mpc_new("doge");
+  mpc_result_t r;
 
-  mpca_lang(MPCA_LANG_DEFAULT, "                                          \
-  adjective : \"wow\" | \"many\" | \"so\" | \"such\"                    \
-  noun      : \"lisp\" | \"language\" | \"c\" | \"book\" | \"build\"    \
-  phrase    : <adjective> <noun>                                        \
-  doge      : <phrase>*;                                                \
-",
+  mpca_lang(MPCA_LANG_DEFAULT, "                                                \
+            adjective : \"wow\" | \"many\" | \"so\" | \"such\";                 \
+            noun      : \"lisp\" | \"language\" | \"c\" | \"book\" | \"build\"; \
+            phrase    : <adjective> <noun>;                                     \
+            doge      : <phrase>*;                                              ",
             Adjective, Noun, Phrase, Doge);
+
+  while (1) {
+    char * input = readline("doge is stupid> ");
+    if (!input) break;
+    add_history(input);
+
+    if (mpc_parse("<stdin>", input, Doge, &r)) {
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    } else {
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
+
+    free(input);
+  }
 
   mpc_cleanup(4, Adjective, Noun, Phrase, Doge);
 }
