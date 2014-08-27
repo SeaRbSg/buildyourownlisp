f413ed6c78226e410930770e055abbdb24581898
Ryan Davis <ryand-ruby@zenspider.com>
2014-08-26 20:12:49 -0700

    Got ch05.c compiling. Still doesn't do anything.

--- zenspider/ch05.c
+++ zenspider/ch05.c
@@ -1,21 +1,18 @@
+#include "mpc.h"
 
-mpc_parser_t* Adjective = mpc_or(4,
-                                 mpc_sym("wow"),
-                                 mpc_sym("many"),
-                                 mpc_sym("so"),
-                                 mpc_sym("such"));
+int main() {
+  mpc_parser_t* Adjective = mpc_new("adjective");
+  mpc_parser_t* Noun      = mpc_new("noun");
+  mpc_parser_t* Phrase    = mpc_new("phrase");
+  mpc_parser_t* Doge      = mpc_new("doge");
 
-mpc_parser_t* Noun = mpc_or(5,
-                            mpc_sym("lisp"),
-                            mpc_sym("language"),
-                            mpc_sym("c"),
-                            mpc_sym("book"),
-                            mpc_sym("build"));
+  mpca_lang(MPCA_LANG_DEFAULT, "                                          \
+  adjective : \"wow\" | \"many\" | \"so\" | \"such\"                    \
+  noun      : \"lisp\" | \"language\" | \"c\" | \"book\" | \"build\"    \
+  phrase    : <adjective> <noun>                                        \
+  doge      : <phrase>*;                                                \
+",
+            Adjective, Noun, Phrase, Doge);
 
-mpc_parser_t* Phrase = mpc_and(2,
-                               mpcf_strfold,
-                               Adjective,
-                               Noun,
-                               free);
-
-mpc_parser_t* Doge = mpc_many(mpc_strfold, Phrase);
+  mpc_cleanup(4, Adjective, Noun, Phrase, Doge);
+}
