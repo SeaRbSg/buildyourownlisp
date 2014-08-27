1c14b225269a358577bdb17ee9f1ca31b0f929ac
Ryan Davis <ryand-ruby@zenspider.com>
2014-08-26 19:22:27 -0700

    Added ch04.c

---
+++ zenspider/ch04.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define BUFSIZE 2048
+static char input[BUFSIZE];
+
+int main(int argc, char** argv) {
+  puts("Lispy version 1.0.0");
+  puts("Press ^c to exit\n");
+
+  while (1) {
+    printf("lispy> ");
+    fgets(input, BUFSIZE, stdin);
+    printf("No you're a %s\n", input);
+  }
+
+  return 0;
+}
