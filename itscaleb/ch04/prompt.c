f982b85982724b93aab882a07e22cdec921d8a04
Caleb Gossler <caleb.gossler@gmail.com>
2014-08-29 22:38:02 -0700

    using readline

--- itscaleb/ch04/prompt.c
+++ itscaleb/ch04/prompt.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-static char input[2048];
+#include <editline/readline.h>
+#include <editline/history.h>
 
 int main(int argc, char** argv) {
 
@@ -8,9 +10,13 @@ int main(int argc, char** argv) {
 	puts("Press Ctrl+c to Exit\n");
 
 	while(1) {
-		fputs("lispy> ", stdout);
-		fgets(input, 2048, stdin);
-		printf("No, you're a %s", input);
+		char* input = readline("lispy> ");
+
+		add_history(input);
+
+		printf("No, you're a %s\n", input);
+
+		free(input);
 	}
 
 	return 0;
