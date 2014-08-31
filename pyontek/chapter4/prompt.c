55a34dc0c0e6fe700e7f4bebb9b51cfec5c81c16
pyontek <kpyontek@gmail.com>
2014-08-31 16:26:54 -0700

    chapter4 complete

--- pyontek/chapter4/prompt.c
+++ pyontek/chapter4/prompt.c
@@ -1,19 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+#ifdef _WIN32
+
+#include <string.h>
 
 //declare static buffer "input" for user input
 static char input[2048];
 
+//readline for windows: does not allow use of arrow keys
+char* readline(char* prompt) {
+      fputs("rhl> ", stdout);
+      fgets(input, 2048, stdin);
+      char* cpy = malloc(strlen(input)+1);
+      strcpy(cpy, input);
+      cpy[strlen(cpy)-1] = '\0';
+      return cpy;
+}
+
+//faux add_history for windows
+void add_history(char* unused) {}
+
+#else
+
+// to allow for use of arrow keys and prior input access in prompt
+#include <editline/readline.h>
+
+#endif
+
 int main(int argc, char** argv){
   
-  // welcome in program
   puts("Rustic Homemade Lisp-thing version 0.0.0.0.1");
   puts("Press Crtl+c to exit\n");
 
   // input loop
   while (1){
-    fputs("rhl> ", stdout);
-    fgets(input, 2048, stdin);
-    printf("yay %s", input);
+    
+    // output prompt text and get input
+    char* input = readline("rhl> ");
+    
+    add_history(input);
+    
+    printf("yay %s\n", input);
+
+    free(input);
   }
   return 0;
 }
