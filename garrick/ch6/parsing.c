55d97ba39c53943331a2f1074e5ca23aec5c503d
Garrick West <garrick@acm.org>
2014-09-07 22:14:05 -0700

    Beginning Chapter 6 set-up

---
+++ garrick/ch6/parsing.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <editline/readline.h>
+#include <editline/history.h>
+#include "mpc.h"
+
+int main(int argc, char** argv) {
+
+  /* Print version and exit */
+  puts("Lithp Version 0.0.0.0.1");
+  puts("Suitable for absolutely nothing!");
+  puts("Press Ctrl-C to exit\n");
+
+  /* never ending storrr-y! */
+  while(1) {
+      /* output our prompt and get input */ 
+      char* input = readline("lithp> ");
+      /* add to our history */
+      add_history(input);
+      /* echo back to user */
+      printf("You said: %s\n", input); 
+      /* free the malocs! */
+      free(input);
+  }
+  return 0;
+}
+
+
