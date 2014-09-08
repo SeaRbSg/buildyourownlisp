aab69797d7bb5635937775a266b6611bf3695c5f
Garrick West <garrick@acm.org>
2014-09-07 22:56:48 -0700

    Parsing working.  No thanks to busted PDF output...

--- garrick/ch6/parsing.c
+++ garrick/ch6/parsing.c
@@ -4,7 +4,24 @@
 #include <editline/history.h>
 #include "mpc.h"
 
+
 int main(int argc, char** argv) {
+
+  /* Create some parsers */
+  mpc_parser_t* Number = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr = mpc_new("expr");
+  mpc_parser_t* Lithp = mpc_new("lithp");
+
+  /* Define them with the following language */
+  mpca_lang(MPCA_LANG_DEFAULT, 
+    "\
+    number : /-?[0-9]+/ ; \
+    operator : '+' | '-' | '*' | '/' ; \
+    expr : <number> | '(' <operator> <expr>+ ')' ; \
+    lithp : /^/ <operator> <expr>+ /$/ ; \
+    ",
+  Number, Operator, Expr, Lithp);
 
   /* Print version and exit */
   puts("Lithp Version 0.0.0.0.1");
@@ -17,12 +34,22 @@ int main(int argc, char** argv) {
       char* input = readline("lithp> ");
       /* add to our history */
       add_history(input);
-      /* echo back to user */
-      printf("You said: %s\n", input); 
+      /* Attempt to parse the user input */
+      mpc_result_t r;
+      if (mpc_parse("<stdin>", input, Lithp, &r)) {
+        /* On success, print the AST */
+        mpc_ast_print(r.output);
+        mpc_ast_delete(r.output);
+      } else {
+        /* Otherwise, print the error */
+        mpc_err_print(r.error);
+        mpc_err_delete(r.error);
+      }
       /* free the malocs! */
       free(input);
   }
+  /* Undefine and Delete our Parsers.  Or else... */
+  mpc_cleanup(4, Number, Operator, Expr, Lithp);
   return 0;
 }
-
 
