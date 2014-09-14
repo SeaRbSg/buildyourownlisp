9bc51751f2377f1785c8ff8ce37d2099359d2d81
Ryan Davis <ryand-ruby@zenspider.com>
2014-09-14 16:12:32 -0700

    Finished ch08 vanilla.

--- zenspider/ch08.c
+++ zenspider/ch08.c
@@ -3,26 +3,87 @@
 #include <string.h>
 #include <math.h>
 
-long eval_op(long x, char* op, long y) {
-  if (strcmp(op, "+") == 0) { return x + y; }
-  if (strcmp(op, "-") == 0) { return x - y; }
-  if (strcmp(op, "*") == 0) { return x * y; }
-  if (strcmp(op, "/") == 0) { return x / y; }
-  if (strcmp(op, "%") == 0) { return x % y; }
-  if (strcmp(op, "^") == 0) { return pow(x, y); }
-  if (strcmp(op, "min") == 0) { return x <= y ? x : y; }
-  if (strcmp(op, "max") == 0) { return x >= y ? x : y; }
-  fprintf(stderr, "WARNING: unknown operator '%s'\n", op);
-  return 0;
+enum { LVAL_NUM, LVAL_ERR };
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+typedef struct lval {
+  int type;
+  long num;
+  int err;
+} lval;
+
+lval lval_num(long x) {
+  lval v;
+  v.type = LVAL_NUM;
+  v.num = x;
+  return v;
 }
 
-long eval(mpc_ast_t* t) {
+lval lval_err(int x) {
+  lval v;
+  v.type = LVAL_ERR;
+  v.err = x;
+  return v;
+}
+
+void lval_print(lval v) {
+  switch (v.type) {
+  case LVAL_NUM:
+    printf("%li", v.num);
+    break;
+  case LVAL_ERR:
+    switch (v.err) {
+    case LERR_DIV_ZERO:
+      printf("Error: Division by zero");
+      break;
+    case LERR_BAD_OP:
+      printf("Error: Invalid operator");
+      break;
+    case LERR_BAD_NUM:
+      printf("Error: Invalid number");
+      break;
+    default:
+      printf("Unknown error type: %d", v.err);
+    }
+    break;
+  default:
+    printf("Unknown lval type: %d", v.type);
+  }
+}
+
+void lval_println(lval v) {
+  lval_print(v);
+  putchar('\n');
+}
+
+lval eval_op(lval x, char* op, lval y) {
+  if (x.type == LVAL_ERR) return x;
+  if (y.type == LVAL_ERR) return y;
+
+  long a = x.num;
+  long b = y.num;
+
+  if (strcmp(op, "+") == 0)   { return lval_num(a + b);          }
+  if (strcmp(op, "-") == 0)   { return lval_num(a - b);          }
+  if (strcmp(op, "*") == 0)   { return lval_num(a * b);          }
+  if (strcmp(op, "/") == 0)   { return b == 0 ? lval_err(LERR_DIV_ZERO) : lval_num(a / b); }
+  if (strcmp(op, "%") == 0)   { return lval_num(a % b);          }
+  if (strcmp(op, "^") == 0)   { return lval_num(pow(a, b));      }
+  if (strcmp(op, "min") == 0) { return lval_num(a <= b ? a : b); }
+  if (strcmp(op, "maa") == 0) { return lval_num(a >= b ? a : b); }
+
+  fprintf(stderr, "WARNING: unknown operator '%s'\n", op);
+  return lval_err(LERR_BAD_OP);
+}
+
+lval eval(mpc_ast_t* t) {
   if (strstr(t->tag, "number")) {
-    return atoi(t->contents);
+    long x = atoi(t->contents);
+    return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
   }
 
   char* op = t->children[1]->contents;
-  long x   = eval(t->children[2]);
+  lval x   = eval(t->children[2]);
 
   for (int i = 3; strstr(t->children[i]->tag, "expr"); i++) {
     x = eval_op(x, op, eval(t->children[i]));
@@ -67,8 +128,9 @@ int main() {
     if (mpc_parse("<stdin>", input, Lispy, &r)) {
       mpc_ast_print(r.output);
 
-      long result = eval(r.output);
-      printf("= %li\n", result);
+      lval result = eval(r.output);
+      printf("= ");
+      lval_println(result);
       printf("# %li\n", count_leaves(r.output));
 
       mpc_ast_delete(r.output);
@@ -76,7 +138,6 @@ int main() {
       mpc_err_print(r.error);
       mpc_err_delete(r.error);
     }
-
 
     free(input);
   }
