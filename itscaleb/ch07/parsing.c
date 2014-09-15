e21c7ec183056fa651ad86020620d5d78d8abeab
Caleb Gossler <caleb.gossler@gmail.com>
2014-09-14 23:21:25 -0700

    finished basic ch07

--- itscaleb/ch07/parsing.c
+++ itscaleb/ch07/parsing.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <editline/readline.h>
-#include <editline/history.h>
 #include "mpc.h"
 
+long eval_op(long x, char* op, long y) {
+	if (strcmp(op, "+") == 0) { return x + y; }
+	if (strcmp(op, "-") == 0) { return x - y; }
+	if (strcmp(op, "*") == 0) { return x * y; }
+	if (strcmp(op, "/") == 0) { return x / y; }
+	return 0;
+}
+
+long eval(mpc_ast_t* t) {
+	if (strstr(t->tag, "number")) {
+		return atoi(t->contents);
+	}
+
+	char* op = t->children[1]->contents;
+
+	long x = eval(t->children[2]);
+
+	int i = 3;
+	while (strstr(t->children[i]->tag, "expr")) {
+		x = eval_op(x, op, eval(t->children[i]));
+		i++;
+	}
+
+	return x;
+}
 
 int main(int argc, char** argv) {
 	mpc_parser_t* Number = mpc_new("number");
@@ -30,7 +55,8 @@ int main(int argc, char** argv) {
 
 		mpc_result_t r;
 		if(mpc_parse("<stdin>", input, Lispy, &r)) {
-			mpc_ast_print(r.output);
+			long result = eval(r.output);
+			printf("%li\n", result);
 			mpc_ast_delete(r.output);
 		} else {
 			mpc_err_print(r.error);
