98e8b57ba34ed92c1a715ae6160aae14387ece9b
Scott Windsor <swindsor@gmail.com>
2014-09-16 11:57:27 -0700

    Adding Chapter 7 unary minus operator

--- sentientmonkey/ch7/evaluation.c
+++ sentientmonkey/ch7/evaluation.c
@@ -29,13 +29,17 @@ long eval(mpc_ast_t* t) {
     if (strstr(t->tag, "number")) { return atoi(t->contents); }
 
     char* op = t->children[1]->contents;
-
     long x = eval(t->children[2]);
 
     int i=3;
-    while (strstr(t->children[i]->tag, "expr")) {
-        x = eval_op(x, op, eval(t->children[i]));
-        i++;
+
+    if (t->children_num == i+1) {
+        x = eval_unary(x, op);
+    } else {
+        while (strstr(t->children[i]->tag, "expr")) {
+            x = eval_op(x, op, eval(t->children[i]));
+            i++;
+        }
     }
 
     return x;
