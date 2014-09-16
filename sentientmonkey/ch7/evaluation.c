bac533d788eea17654d99c5e2c582d3be8a49f86
Scott Windsor <swindsor@gmail.com>
2014-09-16 11:28:07 -0700

    Chapter 7

---
+++ sentientmonkey/ch7/evaluation.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <editline/readline.h>
+#include "mpc.h"
+
+long eval_op(long x, char* op, long y) {
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
+
+    return 0;
+}
+
+long eval(mpc_ast_t* t) {
+    if (strstr(t->tag, "number")) { return atoi(t->contents); }
+
+    char* op = t->children[1]->contents;
+
+    long x = eval(t->children[2]);
+
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+}
+
+int main(int argc, char** argv) {
+    puts("Eval Version 0.0.1");
+    puts("Press Ctrl+d to Exit\n");
+
+    mpc_parser_t* Number   = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr     = mpc_new("expr");
+    mpc_parser_t* Lispy    = mpc_new("lispy");
+
+    mpca_lang(MPCA_LANG_DEFAULT,
+            "                                                                       \
+            number   : /-?[0-9]+(\\.[0-9]+)?/ ;                                     \
+            operator : '+' | '-' | '*' | '/' | '%' | \"add\" | \"mul\" | \"div\" ;  \
+            expr     : <number> | '(' <operator> <expr>+ ')' ;                      \
+            lispy    : /^/ <operator> <expr>+ /$/ ;                                 \
+            ",
+            Number, Operator, Expr, Lispy);
+
+    while(1) {
+        char* input = readline("eval> ");
+        add_history(input);
+        if (!input) { break; }
+
+        mpc_result_t r;
+
+        if (mpc_parse("<stdin>", input, Lispy, &r)) {
+            long result = eval(r.output);
+            printf("%li\n", result);
+            mpc_ast_delete(r.output);
+        } else {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
+
+        free(input);
+    }
+
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
+    return 0;
+}
