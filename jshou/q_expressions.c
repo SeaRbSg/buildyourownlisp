4200f9ccb69233d4ba8a3e127028a4603066c304
jshou <joshuahou@gmail.com>
2014-09-17 15:25:23 -0700

    put forward declarations in a header file

--- jshou/q_expressions.c
+++ jshou/q_expressions.c
@@ -1,48 +1,9 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include "mpc.h"
-
-typedef struct lval {
-  int type;
-  long num;
-
-  char* err;
-  char* sym;
-
-  int count; // count and pointer to list of lval*
-  struct lval** cell;
-} lval;
+#include "q_expressions.h"
 
 enum { LVAL_NUM, LVAL_ERR, LVAL_SYM, LVAL_SEXPR, LVAL_QEXPR }; // lval_types
-
-lval* lval_abstract();
-lval* lval_num(long x);
-lval* lval_err(char* m);
-lval* lval_sym(char* x);
-lval* lval_sexpr(void);
-lval* lval_qepxr(void);
-void lval_del(lval* v);
-lval* lval_add(lval* v, lval* x);
-lval* lval_read_num(mpc_ast_t* t);
-lval* lval_read(mpc_ast_t* t);
-void lval_expr_print(lval* v, char open, char close);
-void lval_print(lval* v);
-void lval_println(lval* v);
-lval* lval_pop(lval* v, int i);
-lval* lval_take(lval* v, int i);
-lval* builtin_op(lval* a, char* op);
-lval* lval_eval(lval* v);
-lval* lval_eval_sexpr(lval* y);
-lval* builtin(lval* a, char* func);
-lval* builtin_head(lval* a);
-lval* builtin_tail(lval* a);
-lval* builtin_list(lval* a);
-lval* builtin_eval(lval* a);
-lval* builtin_join(lval* a);
-lval* lval_join(lval* x, lval* y);
-
-#define LASSERT(args, cond, err) if (!(cond)) { lval_del(args); return lval_err(err); }
-
 
 lval* lval_abstract() {
   lval* lv = malloc(sizeof(lval));
---
+++ jshou/q_expressions.h
@@ -0,0 +1,38 @@
+typedef struct lval {
+  int type;
+  long num;
+
+  char* err;
+  char* sym;
+
+  int count; // count and pointer to list of lval*
+  struct lval** cell;
+} lval;
+
+lval* lval_abstract();
+lval* lval_num(long x);
+lval* lval_err(char* m);
+lval* lval_sym(char* x);
+lval* lval_sexpr(void);
+lval* lval_qepxr(void);
+void lval_del(lval* v);
+lval* lval_add(lval* v, lval* x);
+lval* lval_read_num(mpc_ast_t* t);
+lval* lval_read(mpc_ast_t* t);
+void lval_expr_print(lval* v, char open, char close);
+void lval_print(lval* v);
+void lval_println(lval* v);
+lval* lval_pop(lval* v, int i);
+lval* lval_take(lval* v, int i);
+lval* builtin_op(lval* a, char* op);
+lval* lval_eval(lval* v);
+lval* lval_eval_sexpr(lval* y);
+lval* builtin(lval* a, char* func);
+lval* builtin_head(lval* a);
+lval* builtin_tail(lval* a);
+lval* builtin_list(lval* a);
+lval* builtin_eval(lval* a);
+lval* builtin_join(lval* a);
+lval* lval_join(lval* x, lval* y);
+
+#define LASSERT(args, cond, err) if (!(cond)) { lval_del(args); return lval_err(err); }
