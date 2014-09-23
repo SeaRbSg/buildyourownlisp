52001f5925dad2cd66b6c33c7e397534131a2ab6
Aja Hammerly <aja.hammerly+github@gmail.com>
2014-09-22 18:18:28 -0700

    Infix

--- thagomizer/ch6/parsing.c
+++ thagomizer/ch6/parsing.c
@@ -16,7 +16,7 @@ int main() {
             number :   /-?[0-9]+(.[0-9]+)?/ ;                                \
             operator:   '+' | '-' | '*' | '/' | '%' | /add/ | /sub/ | /mul/ | /div/ ;      \
             expr : <number> | '(' <operator> <expr>+ ')' ;         \
-            lispy : /^/ <operator> <expr>+ /$/ ;                   \
+            lispy : /^/ <expr> (<operator> <expr>)+ /$/ ;                   \
             ",
             Number, Operator, Expr, Lispy);
 
