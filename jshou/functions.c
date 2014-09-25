68f6e2619a0d9d084a7c29a307b98d4dccd5f99f
jshou <joshuahou@gmail.com>
2014-09-25 16:18:48 -0700

    if & remains, bind next symbol to empty list

--- jshou/functions.c
+++ jshou/functions.c
@@ -499,6 +499,21 @@ lval* lval_call(lenv* e, lval* f, lval* a) {
 
   lval_del(a);
 
+  // if '&' remains, it should be bound to empty list
+  if (f->formals->count > 0 && strcmp(f->formals[0]->sym, "&") == 0) {
+    if (f->formals->count != 2) {
+      return lval_err("Function format invalid. Symbol '&' not followed by single symbol.");
+    }
+
+    lval_del(lval_pop(f->formals, 0)); // delete '&'
+    lval* sym = lval_pop(f->formals, 0);
+    lval* val = lval_qexpr();
+
+    lenv_put(f->env, sym, val);
+    lval_del(sym);
+    lval_del(val);
+  }
+
   if (f->formals->count == 0) {
     f->env->par = e;
     return builtin_eval(f->env, lval_add(lval_sexpr(), lval_copy(f->body)));
