8f201555fcb3e075056b2a308b5a8ab25fbc2c78
Caleb Gossler <caleb.gossler@gmail.com>
2014-09-29 23:26:29 -0700

    finished eval

--- itscaleb/ch09/parsing.c
+++ itscaleb/ch09/parsing.c
@@ -126,50 +126,94 @@ void lval_print(lval* val) {
 
 void lval_println(lval* v) { lval_print(v); putchar('\n'); }
 
+lval* lval_pop(lval* v, int i) {
+  lval* x = v->cell[i];
 
-/* lval eval_op(lval x, char* op, lval y) { */
-/*   if (x.type == LVAL_ERR) { return x; } */
-/*   if (y.type == LVAL_ERR) { return y; } */
-/*   if (strcmp(op, "+") == 0) { return lval_num(x.num + y.num); } */
-/*   if (strcmp(op, "-") == 0) { return lval_num(x.num - y.num); } */
-/*   if (strcmp(op, "*") == 0) { return lval_num(x.num * y.num); } */
-/*   if (strcmp(op, "/") == 0) { */
-/*     return y.num == 0 ? lval_err(LERR_DIV_ZERO) : lval_num(x.num / y.num);  */
-/*   } */
-/*   if (strcmp(op, "%") == 0) {  */
-/*     return y.num == 0 ? lval_err(LERR_DIV_ZERO) : lval_num(x.num % y.num);  */
-/*   } */
-/*   if (strcmp(op, "^") == 0) { return lval_num(pow(x.num, y.num)); } */
-/*   if (strcmp(op, "min") == 0) { return x.num < y.num ? lval_num(x.num) : lval_num(y.num); } */
-/*   if (strcmp(op, "max") == 0) { return x.num > y.num ? lval_num(x.num) : lval_num(y.num); } */
+  memmove(&v->cell[i], &v->cell[i+1], sizeof(lval*) * (v->count-i-1));
 
-/*   return lval_err(LERR_BAD_OP); */
-/* } */
+  v->count--;
 
-/* lval eval(mpc_ast_t* t) { */
-/*   if (strstr(t->tag, "number")) { */
-/*     errno = 0; */
-/*     long x = strtol(t->contents, NULL, 10); */
-/*     return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM); */
-/*   } */
+  v->cell = realloc(v->cell, sizeof(lval*) * v->count);
+  return x;
+}
 
-/*   char* op = t->children[1]->contents; */
+lval* lval_take(lval* v, int i) {
+  lval* x = lval_pop(v, i);
+  lval_del(v);
+  return x;
+}
 
-/*   lval x = eval(t->children[2]); */
+lval* builtin_op(lval* a, char* op) {
+  
+  for (int i = 0; i < a->count; i++) {
+    if (a->cell[i]->type != LVAL_NUM) {
+      lval_del(a);
+      return lval_err("Cannot operate on non-number!");
+    }
+  }
+  
+  lval* x = lval_pop(a, 0);
 
-/*   int i = 3; */
-/*   while (strstr(t->children[i]->tag, "expr")) { */
-/*     x = eval_op(x, op, eval(t->children[i])); */
-/*     i++; */
-/*   } */
+  if ((strcmp(op, "-") == 0) && a->count == 0) { x->num = -x->num; }
 
-/*   /\* if '-' has only one argument, negate it. *\/ */
-/*   if(strcmp(op, "-") == 0 && i == 3) { */
-/*     x.num = x.num * -1; */
-/*   } */
+  while (a->count > 0) {
 
-/*   return x; */
-/* } */
+    lval* y = lval_pop(a, 0);
+
+    if (strcmp(op, "+") == 0) { x->num += y->num; }
+    if (strcmp(op, "-") == 0) { x->num -= y->num; }
+    if (strcmp(op, "*") == 0) { x->num *= y->num; }
+    if (strcmp(op, "/") == 0) {
+      if (y->num == 0) {
+        lval_del(x); lval_del(y);
+        x = lval_err("Division By Zero!"); break;
+      }
+      x->num /= y->num;
+    }
+
+    if (strcmp(op, "%") == 0) { 
+	       	return y->num == 0 ? lval_err("Division By Zero!") : lval_num(x->num % y->num); 
+    }
+    if (strcmp(op, "^") == 0) { x->num = pow(x->num, y->num); }
+    if (strcmp(op, "min") == 0) { x->num = x->num < y->num ? x->num : y->num; }
+    if (strcmp(op, "max") == 0) { x-> num = x->num > y->num ? x->num : y->num; }
+
+    lval_del(y);
+  }
+
+  lval_del(a);
+  return x;
+}
+
+lval* lval_eval(lval* v);
+
+lval* lval_eval_sexpr(lval* v) {
+  for(int i = 0; i < v->count; i++) {
+    v->cell[i] = lval_eval(v->cell[i]);
+  }
+
+  for(int i = 0; i < v->count; i++) {
+    if (v->cell[i]->type == LVAL_ERR) { return lval_take(v, i); }
+  }
+
+  if (v->count == 0) { return v; }
+  if (v->count == 1) { return lval_take(v, 0); }
+  
+  lval* f = lval_pop(v, 0);
+  if (f->type != LVAL_SYM) {
+    lval_del(f); lval_del(v);
+    return lval_err("S-expression Does not start with symbol!");
+  }
+
+  lval* result = builtin_op(v, f->sym);
+  lval_del(f);
+  return result;
+}
+
+lval* lval_eval(lval* v) {
+  if (v->type == LVAL_SEXPR) { return lval_eval_sexpr(v); }
+  return v;
+}
 
 int main(int argc, char** argv) {
   mpc_parser_t* Number	= mpc_new("number");
@@ -199,7 +243,7 @@ int main(int argc, char** argv) {
 
     mpc_result_t r;
     if(mpc_parse("<stdin>", input, Lispy, &r)) {
-      lval* x = lval_read(r.output);
+      lval* x = lval_eval(lval_read(r.output));
       lval_println(x);
       lval_del(x);
     } else {
