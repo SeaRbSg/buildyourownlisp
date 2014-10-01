193ed0990db4719c2fc0e6b7c40103e79ea47bb4
Garrick West <garrick@acm.org>
2014-10-01 13:05:14 -0700

    Finally finishing Ch9.  No valgrind leaks though:)

--- garrick/ch9/sexpressions.c
+++ garrick/ch9/sexpressions.c
@@ -28,6 +28,10 @@ typedef struct lval {
 
 /* forward dec. */
 void lval_print(lval* v);
+lval* lval_pop(lval* v, int i);
+lval* lval_take(lval* v, int i);
+lval* lval_eval(lval* v);
+lval* builtin_op(lval* a, char* op);
 
 /* pointer to new number type lval */
 lval* lval_num(long x) {
@@ -110,7 +114,7 @@ lval* lval_read(mpc_ast_t* t) {
   /* If root (>) or sexpr then create empty list*/
   lval* x = NULL;
   if (strcmp(t->tag, ">") == 0) { x = lval_sexpr(); }
-  if (strstr(t->tag, "sexpr") == 0) { x = lval_sexpr(); }
+  if (strstr(t->tag, "sexpr"))  { x = lval_sexpr(); }
 
   /* Fill this list with any valid expression contained within */
   for (int i = 0; i < t->children_num; i++) {
@@ -177,53 +181,106 @@ lval* max(lval* x, lval* y) {
   return y;
 }
 
-/* Use operator string to see which operator to perform */
-/*
-lval eval_op(lval x, char* op, lval y) {
-  * bail out on errors*
-  if (x.type == LVAL_ERR) { return x; }
-  if (y.type == LVAL_ERR) { return y; }
+lval* lval_eval_sexpr(lval* v) {
 
-  * math up the numbers *
-  if (strcmp(op, "+") == 0) { return lval_num(x.num + y.num); }
-  if (strcmp(op, "-") == 0) { return lval_num(x.num - y.num); }
-  if (strcmp(op, "*") == 0) { return lval_num(x.num * y.num); }
-  if (strcmp(op, "/") == 0) { 
-      return y.num == 0 ? lval_err(LERR_DIV_ZERO) : lval_num(x.num / y.num); 
-    }
-  if (strcmp(op, "%") == 0) { return lval_num(x.num % y.num); }
-  if (strcmp(op, "^") == 0) { return our_exponent( x, y); }
-  if (strcmp(op, "min") == 0) { return min( x, y); }
-  if (strcmp(op, "max") == 0) { return max( x, y); }
+  /* Evaluate children */
+  for (int i = 0; i < v->count; i++) {
+    v->cell[i] = lval_eval(v->cell[i]);
+  }
 
-  * wtf lol idk *
-  return lval_err(LERR_BAD_OP);
+  /* Error checking */
+  for (int i = 0; i < v->count; i++) {
+    if (v->cell[i]->type == LVAL_ERR) { return lval_take(v, i); }
+  }
+
+  /* Empty expression */
+  if (v->count == 0) { return v;}
+
+  /* Single expression */
+  if (v->count == 1) { return lval_take(v, 0);}
+
+  /* Ensure First element is Symbol */
+  lval* f = lval_pop(v, 0);
+  if (f->type != LVAL_SYM) {
+    lval_del(f); lval_del(v);
+    return lval_err("s-expression Does not start with symbol!");
+  }
+
+
+  /* Call builtin with operator */
+  lval* result = builtin_op(v, f->sym);
+  lval_del(f);
+  return result;
 }
-*/
 
 /* Let us eval */
-/*
-lval eval(mpc_ast_t* t) {
+lval* lval_eval(lval* v) {
+  /* Evaluate Sexpressions */
+  if (v->type == LVAL_SEXPR) { return lval_eval_sexpr(v); }
+  /* All other lval types remain the same */
+  return v;
+}
 
-  if (strstr(t->tag, "number")) {
-    * check for an error in conversion *
-    errno = 0;
-    long x = strtol(t->contents, NULL, 10);
-    return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM); 
-  }
+lval* lval_pop(lval* v, int i) {
+  /* Find the item at "i" */
+  lval* x = v->cell[i];
 
-  char* op = t->children[1]->contents;
-  lval x = eval(t->children[2]);
+  /* Shift the memory following the item at "i" over the top of it */
+  memmove(&v->cell[i], &v->cell[i+1], sizeof(lval*) * (v->count-i-1));
 
-  int i = 3;
-  while(strstr(t->children[i]->tag, "expr")) {
-    x = eval_op(x, op, eval(t->children[i]));
-    i++;
-  }
+  /* Decrease the count of items in the list */
+  v->count--;
 
+  /*Reallocate the memory used */
+  v->cell = realloc(v->cell, sizeof(lval*) * v->count);
   return x;
 }
-*/
+
+lval* lval_take(lval* v, int i) {
+  lval* x = lval_pop(v, i);
+  lval_del(v);
+  return x;
+}
+
+lval* builtin_op(lval* a, char* op) {
+  /* Ensure all arguments are numbers */
+  for (int i = 0; i < a->count; i++) {
+    if(a->cell[i]->type != LVAL_NUM) {
+      lval_del(a);
+      return lval_err("Cannot operate on a non-number!");
+    }
+  }
+  /* Pop the first element */
+  lval* x = lval_pop(a, 0);
+
+  /* If no argument and sub then perform unary negation */
+  if ((strcmp(op, "-") == 0) && a->count == 0) { x->num = -x->num; }
+
+  /* While there are still elements remaining */
+  while (a->count > 0) {
+    /* Pop the next element */
+    lval* y = lval_pop(a, 0);
+
+    /* perform the next operation */
+    if (strcmp(op, "+") == 0) { x->num += y->num; }
+    if (strcmp(op, "-") == 0) { x->num -= y->num; }
+    if (strcmp(op, "*") == 0) { x->num *= y->num; }
+    if (strcmp(op, "/") == 0) {
+      if (y->num == 0) {
+        lval_del(x);
+        lval_del(y);
+        x = lval_err("Division by Zero!");
+        break;
+      }
+      x->num /= y->num;
+    }
+    /* Delete element no that we're finished with it */
+    lval_del(y);
+  }
+  /* delete input expression and return result */
+  lval_del(a);
+  return x;
+}
 
 int main(int argc, char** argv) {
 
@@ -263,9 +320,9 @@ int main(int argc, char** argv) {
         /* On success, print the AST */
         //mpc_ast_print(r.output);
         /* Get a real result and print it */
-        lval* result = lval_read(r.output);
-        lval_println(result);
-        lval_del(r.output);
+        lval* x = lval_eval(lval_read(r.output));
+        lval_println(x);
+        lval_del(x);
       } else {
         /* Otherwise, print the error */
         mpc_err_print(r.error);
