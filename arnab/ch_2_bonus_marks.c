8660cd53c7a10fbad759eff97acb8519bcbb0ccd
Arnab Deka <arnab.deka@gmail.com>
2014-09-30 20:25:39 -0700

    rest of ch 3 bonus points.

--- arnab/ch_2_bonus_marks.c
+++ arnab/ch_2_bonus_marks.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 
+void say_hello(int n){
+  int i;
+  for (i = 0; i < n; i++) {
+    puts("Hello World!");
+  }
+}
+
 int main(int argc, char *argv[])
 {
+  puts("##### for loop #####");
   int i;
   for (i = 0; i < 5; i++) {
     puts("Hello World!");
   }
+
+  puts("\n##### while loop #####");
+  i = 0;
+  while (i < 5) {
+    puts("Hello World!");
+    i++;
+  }
+
+  puts("\n##### using a fn ######");
+  say_hello(5);
 
   return 0;
 }
