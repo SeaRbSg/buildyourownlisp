65827f44a641601c57a99b12c1e91929b40163d3
Aja Hammerly <aja.hammerly+github@gmail.com>
2014-10-21 20:20:31 -0700

    Chapter 13

---
+++ thagomizer/ch13/lispy.c
@@ -0,0 +1,11 @@
+  /Users/aja/Projects/buildyourownlisp/thagomizer/ch12:
+  total used in directory 528 available 35762448
+  drwxr-xr-x   9 aja  staff     306 Oct 21 20:19 .
+  drwxr-xr-x  17 aja  staff     578 Oct 21 19:32 ..
+  -rw-r--r--   1 aja  staff     130 Oct 18 18:11 Makefile
+  -rwxr-xr-x   1 aja  staff  117604 Oct 21 20:19 lispy
+  -rw-r--r--   1 aja  staff   23126 Oct 21 20:18 lispy.c
+D -rw-r--r--   1 aja  staff   23049 Oct 21 19:59 lispy.c~
+  drwxr-xr-x   3 aja  staff     102 Oct 21 20:19 lispy.dSYM
+  -rw-r--r--   1 aja  staff   83079 Oct 18 18:11 mpc.c
+  -rw-r--r--   1 aja  staff    8729 Oct 18 18:11 mpc.h
