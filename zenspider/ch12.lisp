56753c649751e47c5087d946c49cf52bb960fa2e
Ryan Davis <ryand-ruby@zenspider.com>
2014-10-28 13:13:42 -0700

    added ch12.lisp

---
+++ zenspider/ch12.lisp
@@ -0,0 +1,19 @@
+def {fun} (lambda {args body} {def (head args) (lambda (tail args) body)})
+
+{add-together}
+fun {add-together x y} {+ x y}
+add-together 2 3
+
+{unpack}
+fun {unpack f xs} {eval (join (list f) xs)}
+fun {pack f & xs} {f xs}
+def {uncurry} pack
+def {curry} unpack
+
+curry + {5 6 7}
+uncurry head 5 6 7
+
+def {add-uncurried} +
+def {add-curried} (curry +)
+add-curried {5 6 7}
+add-uncurried 5 6 7
