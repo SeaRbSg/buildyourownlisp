34c9109c05d3545b74bdc5f70c5603ba8323ad64
Ryan Davis <ryand-ruby@zenspider.com>
2014-10-28 22:59:29 -0700

    sort

--- zenspider/ch14.c
+++ zenspider/ch14.c
@@ -623,30 +623,29 @@ void lenv_add_builtins(lenv *e) {
   lenv_add_builtin(e, "cons",   builtin_cons);
   lenv_add_builtin(e, "def",    builtin_def);
   lenv_add_builtin(e, "/",      builtin_div);
+  lenv_add_builtin(e, "==",     builtin_eq);
   lenv_add_builtin(e, "eval",   builtin_eval);
   lenv_add_builtin(e, "^",      builtin_exp);
+  lenv_add_builtin(e, ">=",     builtin_ge);
+  lenv_add_builtin(e, ">",      builtin_gt);
   lenv_add_builtin(e, "head",   builtin_head);
+  lenv_add_builtin(e, "if",     builtin_if);
   lenv_add_builtin(e, "join",   builtin_join);
   lenv_add_builtin(e, "lambda", builtin_lambda);
+  lenv_add_builtin(e, ">=",     builtin_le);
   lenv_add_builtin(e, "len",    builtin_len);
   lenv_add_builtin(e, "list",   builtin_list);
   lenv_add_builtin(e, "load",   builtin_load);
+  lenv_add_builtin(e, "<",      builtin_lt);
   lenv_add_builtin(e, "max",    builtin_max);
   lenv_add_builtin(e, "min",    builtin_min);
   lenv_add_builtin(e, "%",      builtin_mod);
   lenv_add_builtin(e, "*",      builtin_mul);
+  lenv_add_builtin(e, "!=",     builtin_ne);
+  lenv_add_builtin(e, "print",  builtin_print);
   lenv_add_builtin(e, "=",      builtin_put);
   lenv_add_builtin(e, "-",      builtin_sub);
   lenv_add_builtin(e, "tail",   builtin_tail);
-  lenv_add_builtin(e, "print",  builtin_print);
-
-  lenv_add_builtin(e, ">",      builtin_gt);
-  lenv_add_builtin(e, "<",      builtin_lt);
-  lenv_add_builtin(e, ">=",     builtin_ge);
-  lenv_add_builtin(e, ">=",     builtin_le);
-  lenv_add_builtin(e, "==",     builtin_eq);
-  lenv_add_builtin(e, "!=",     builtin_ne);
-  lenv_add_builtin(e, "if",     builtin_if);
 }
 
 /*
