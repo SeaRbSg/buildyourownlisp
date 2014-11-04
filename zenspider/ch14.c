8dc44a4a785b19358ef46f53e6eae03ca03e2c50
Ryan Davis <ryand-ruby@zenspider.com>
2014-11-03 22:11:34 -0800

    Fixed parser for symbols.

--- zenspider/ch14.c
+++ zenspider/ch14.c
@@ -1166,7 +1166,7 @@ int main(int argc, char** argv) {
 
   mpca_lang(MPCA_LANG_DEFAULT, "                                \
             number   : /-?[0-9]+(\\.[0-9]+)?/;                  \
-            symbol   : /[a-zA-Z0-9_+*\\/\\\\=<>!&-]+/;          \
+            symbol   : /[a-zA-Z0-9_+*\\/\\\\=<>!&^%-]+/;        \
             string   : /\"(\\\\.|[^\"])*\"/;                    \
             comment  : /;[^\\r\\n]*/;                           \
             sexp     : '(' <expr>* ')';                         \
